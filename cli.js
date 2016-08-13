0f9a8a2a0e75dcc30542d158cab007e218827bdc
akameco <akameco.t@gmail.com>
2016-08-14 08:55:02 +0900

    Update CLI

--- cli.js
+++ cli.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
 'use strict';
+const meow = require('meow');
+const updateNotifier = require('update-notifier');
 const npmSrc = require('./');
 
-npmSrc(process.argv.slice(2))
+const cli = meow(`
+	Usage
+	  $ npm-src <package name> [string, array]
+
+	Examples
+	  $ npm-src ava xo
+`);
+
+if (cli.input.length === 0) {
+	console.error('Specify a package name');
+	process.exit(1);
+}
+
+updateNotifier({pkg: cli.pkg}).notify();
+
+npmSrc(cli.input)
 	.then(results => {
 		results.forEach(x => console.log(x));
 	}).catch(err => {
