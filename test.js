340199107a094d09c72bb7a05e322ca3f07785cf
akameco <akameco.t@gmail.com>
2017-06-07 23:21:28 +0900

    Update deps

--- test.js
+++ test.js
@@ -2,9 +2,17 @@ import test from 'ava';
 import fn from './';
 
 test('throws', async t => {
-	t.throws(fn('co'), /Expected a array/);
+	try {
+		await fn('co');
+	} catch (err) {
+		t.is(err.message, 'Expected a array');
+	}
 });
 
 test('not exist', async t => {
-	t.throws(fn(['@akameco/not-exist']), /not exits. Check package name/);
+	try {
+		await fn(['@akameco/not-exist']);
+	} catch (err) {
+		t.is(err.message, '@akameco/not-exist not exits. Check package name');
+	}
 });
