db4e61365a04d2e4af81efd21eb3ec11aae4ec67
akameco <akameco.t@gmail.com>
2017-06-08 00:04:19 +0900

    Show install url

--- index.js
+++ index.js
@@ -3,25 +3,28 @@ const packageJson = require('package-json')
 const execa = require('execa')
 const Listr = require('listr')
 
-const runGhqGet = repo => execa.stdout('ghq', ['get', '-p', repo])
-
-const clone = repo =>
-	packageJson(repo).then(pkg => pkg.repository).then(repo => {
-		if (repo.type !== 'git') {
-			return Promise.reject(new Error('Expected a git repository'))
-		}
-		return runGhqGet(repo.url)
-	})
+const runGhqGet = repoUrl => execa.stdout('ghq', ['get', '-p', repoUrl])
 
 const ghqTasks = repos => {
 	const tasks = repos.map(repo => ({
 		title: `ghq get ${repo}`,
-		task: () =>
-			clone(repo).catch(() => {
-				return Promise.reject(
-					new Error(`${repo} not exits. Check package name`)
-				)
-			})
+		task: (ctx, task) =>
+			packageJson(repo, { fullMetadata: true })
+				.then(pkg => {
+					task.output = pkg.repository.url
+					return pkg.repository
+				})
+				.then(repo => {
+					if (repo.type !== 'git') {
+						return Promise.reject(new Error('Expected a git repository'))
+					}
+					return runGhqGet(repo.url)
+				})
+				.catch(() => {
+					return Promise.reject(
+						new Error(`${repo} not exits. Check package name`)
+					)
+				})
 	}))
 
 	return new Listr(tasks)
